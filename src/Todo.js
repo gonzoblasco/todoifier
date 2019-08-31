0b81db3d3837ab81f32a785fb2426a272ad6bf71
Gonzalo Blasco <gblasco@minutentag.com>
2019-08-31 12:13:28 -0300

    First Commit

---
+++ src/Todo.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "./Todo.css";
+
+class Todo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      description: props.description,
+      done: false
+    };
+
+    this.markAsDone = this.markAsDone.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
+  }
+
+  cssClasses() {
+    let classes = ["Todo"];
+
+    if (this.state.done) {
+      classes = [...classes, "Done"];
+    }
+
+    return classes.join(" ");
+  }
+
+  markAsDone() {
+    this.setState({ done: true });
+  }
+
+  removeTodo() {
+    this.props.removeTodo(this.state.description);
+  }
+
+  render() {
+    return (
+      <div className={this.cssClasses()}>
+        {this.state.description}
+        <br />
+        <button onClick={this.markAsDone}>Mark as Done</button>
+        <button onClick={this.removeTodo}>Remove Me</button>
+      </div>
+    );
+  }
+}
+export default Todo;
